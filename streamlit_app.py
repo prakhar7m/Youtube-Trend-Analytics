80e26a66eba53afca756eb89cfd4dc115ab97fca
prakhar7m <61321245+prakhar7m@users.noreply.github.com>
2022-11-21 19:35:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -281,7 +281,7 @@ st.markdown("### Final Data preview")
 st.dataframe(df_2.head())
 
 # grab list of states from the dataframe
-features= ['published_hour', 'likes', 'comment_count', 'view_count' , 'published_month'
+features= ['published_hour', 'likes', 'comment_count', 'view_count' , 'published_month']
 
 # selectbox for selecting which state to plot
 feature_selected = st.selectbox(‘Select feature to view’, features, index=(53))
